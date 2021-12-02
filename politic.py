2b8eca039c436978d58858af72618a245a4613f9
bill8345 <93075612+bill8345@users.noreply.github.com>
2021-12-02 16:20:04 +0800

    Update politic.py

--- politic.py
+++ politic.py
@@ -42,19 +42,43 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    #中央社爬蟲#
     url_cent = 'https://www.cna.com.tw/list/aipl.aspx'
     re_cent = requests.get(url_cent)
     soup_cent = BeautifulSoup(re_cent.text, 'html.parser')
     temp = soup_cent.find('ul', {'class': 'mainList imgModule'})
     temp_1 = temp.find_all('li')
+    #中央社爬蟲#
 
+    #關鍵評論網爬蟲#
+    url_lens = 'https://www.thenewslens.com/category/politics'
+    re_lens = requests.get(url_lens)
+    soup_lens = BeautifulSoup(re_lens.text, 'html.parser')
+    temp_1 = soup_lens.find('h2', {'class': 'title'})
+    #關鍵評論網爬蟲#
+
+
+    politic_url = '*請輸入你有興趣得網路媒體*\n端傳媒\n關鍵評論網\n中央社'
     title_politic = temp_1[0].text.replace('2021', '#').split('#')[0]
     html_politic = temp_1[0].a['href']
+    lens_title = temp_1.text.replace(' ', '')
+    lens_html = temp_1.a['href']
     message_text = event.message.text
+
     if message_text == '政治':
-        reply_arr = []
-        reply_arr.append(TextSendMessage(text=title_politic))
-        reply_arr.append(TextSendMessage(text=html_politic))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=politic_url))
+
+    elif message_text == '中央社':
+        reply_arr_cen = []
+        reply_arr_cen.append(TextSendMessage(text=title_politic))
+        reply_arr_cen.append(TextSendMessage(text=html_politic))
+        line_bot_api.reply_message(
+            event.reply_token,
+            reply_arr)
+    elif message_text == '關鍵評論網':
+        reply_arr_lens = []
+        reply_arr_lens.append(TextSendMessage(text=lens_title))
+        reply_arr_lens.append(TextSendMessage(text=lens_html))
         line_bot_api.reply_message(
             event.reply_token,
             reply_arr)

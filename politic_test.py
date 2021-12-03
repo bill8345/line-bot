817a63b05db5b7fc8677a70422b2d69173f518ef
bill8345 <93075612+bill8345@users.noreply.github.com>
2021-12-03 17:39:03 +0800

    Add files via upload

--- politic_test.py
+++ politic_test.py
@@ -9,6 +9,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import jieba
 import configparser
 import requests
 from bs4 import BeautifulSoup
@@ -42,6 +43,31 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    def keyword_title(temp_2):
+        dict_title = {}
+        for temp_3 in temp_2:
+            name_text = temp_3.text.replace(' ', '').replace('「', '').replace('，', '').replace('」', '').replace('：',
+                                                                                                                '').replace(
+                '？', '').replace('【', '').replace('】', '')
+            jieba_title = '\ '.join(jieba.cut(name_text, cut_all=True, HMM=True))
+            list_title = jieba_title.split('\ ')
+            for word in list_title:
+                if word not in dict_title:
+                    dict_title[word] = 1
+                elif word in dict_title:
+                    dict_title[word] = dict_title[word] + 1
+                else:
+                    print('worng')
+        fin_dict_title = sorted(dict_title.items(), key=lambda x: x[1], reverse=True)
+        keyword = []
+
+        for i in range(len(fin_dict_title)):
+            if len(fin_dict_title[i][0]) != 1:
+                keyword.append(fin_dict_title[i])
+        top3_keyword = []
+        for i in range(0, 4):
+            top3_keyword.append(keyword[i][0])
+        return top3_keyword
     #中央社爬蟲#
     url_cent = 'https://www.cna.com.tw/list/aipl.aspx'
     re_cent = requests.get(url_cent)
@@ -54,19 +80,17 @@ def handle_message(event):
     url_lens = 'https://www.thenewslens.com/category/politics'
     re_lens = requests.get(url_lens)
     soup_lens = BeautifulSoup(re_lens.text, 'html.parser')
-    temp_1 = soup_lens.find('h2', {'class': 'title'})
+    temp_2 = soup_lens.find_all('h2', {'class': 'title'})
     #關鍵評論網爬蟲#
 
-
-    politic_url = '*請輸入你有興趣得網路媒體*\n端傳媒\n關鍵評論網\n中央社'
     title_politic = temp_1[0].text.replace('2021', '#').split('#')[0]
     html_politic = temp_1[0].a['href']
-    lens_title = temp_1.text.replace(' ', '')
-    lens_html = temp_1.a['href']
+    lens_title = temp_2[0].text.replace(' ', '')
+    lens_html = temp_2[0].a['href']
     message_text = event.message.text
 
     if message_text == '政治':
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=politic_url))
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='*請輸入你有興趣得網路媒體*\n端傳媒\n關鍵評論網\n中央社'))
 
     elif message_text == '中央社':
         reply_arr_cen = []
@@ -74,14 +98,19 @@ def handle_message(event):
         reply_arr_cen.append(TextSendMessage(text=html_politic))
         line_bot_api.reply_message(
             event.reply_token,
-            reply_arr)
+            reply_arr_cen)
     elif message_text == '關鍵評論網':
         reply_arr_lens = []
         reply_arr_lens.append(TextSendMessage(text=lens_title))
         reply_arr_lens.append(TextSendMessage(text=lens_html))
         line_bot_api.reply_message(
             event.reply_token,
-            reply_arr)
+            reply_arr_lens)
+    elif message_text == '熱門議題':
+        reply_hot = '根據關鍵評論網政治版約{}篇文章標題分析，前三大熱門關鍵字為：\n1. {}\n2. {}\n3. {}\n'.format(len(temp_2),keyword_title(temp_2)[0],keyword_title(temp_2)[1],keyword_title(temp_2)[2])
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_hot))
     else:
         line_bot_api.reply_message(
             event.reply_token,

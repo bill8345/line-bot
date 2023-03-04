60e7584f6dad2978cfb6d011826965cdd38b8123
bill8345 <93075612+bill8345@users.noreply.github.com>
2023-03-04 23:57:00 +0800

    Update caculation.py

--- caculation.py
+++ caculation.py
@@ -105,7 +105,7 @@ def handle_message(event):
         message = TextSendMessage(text=total_count)
         line_bot_api.reply_message(event.reply_token, message)
     elif msg[0] == '$' and int(msg.split('$')[1])>=0:
-        msg=int(msg[1])*0.23
+        msg = int(msg.split('$')[1])*0.23
         temp_1 = py.count('billing', 'count', 'posi')
         temp_2 = py.count('billing', 'count', 'nega')
         temp_1.append(int(msg))
